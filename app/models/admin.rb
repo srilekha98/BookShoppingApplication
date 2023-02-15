64496d39ac329d02810a16782d16e985c53fb6b0
sngudipa <sngudipa@ncsu.edu>
2023-02-15 15:31:56 -0500

    add admin validations

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,5 +1,6 @@
 class Admin < ApplicationRecord
   has_secure_password
-
-  validates :username, presence: true, uniqueness: true
+  validates :name, :email, presence: true
+  validates :username, :password, presence: true, on: [new, create]
+  validates :username, presence: true, uniqueness: {message: 'An account associated with %{value} already exists'}, on: [new, create]
 end
